f0747ea7ae143914b202c429d8230315e088625b
Mario Vela <mariovela22@hotmail.com>
2025-05-27 09:20:04 -0600

    indexado

---
+++ indexado.py
@@ -0,0 +1,10 @@
+producto = "Chocolates"
+
+print(f'{producto[0]}')
+print(f'{producto[-1]}')
+print(f'{producto[:4]}')
+print(f'{producto[-3:]}')
+print(f'{producto[::-1]}')
+print(f'{producto[1:8]}')
+print(f'{producto[::3]}')
+print(len(producto))
