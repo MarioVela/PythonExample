e23476d6a90323334f2b6a170797753559b0e4d4
Mario Vela <mariovela22@hotmail.com>
2025-05-27 20:40:21 -0600

    codigo seguro

--- codigo de seguridad.py
+++ codigo de seguridad.py
@@ -1,2 +1,8 @@
 # codigo de seguridad
-nombre_completo =
+nombre = input('Nombre completo del usuario: ')
+apellido_p = input('Apellido paterno del usuario: ')
+apellido_m = input('Apellido materno del usuario: ')
+curp = input('Ingrese su CURP: ')
+año = input('Año que ingreso a la empresa: ')
+ape_mat = (f'{apellido_m[:3]}')
+print(f'Su codigo de seguridad es: {nombre[2]}{apellido_p[-1]}{ape_mat[::-1]}{curp[-6:]}{nombre[0:8:2]}{año[-2:]}')
