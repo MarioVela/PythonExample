4dc6cdd580e7b71d6400cb31029d0028bdd7795e
Mario Vela <mariovela22@hotmail.com>
2025-06-07 19:44:21 -0600

    python

---
+++ python2/Ejercicio1.py
@@ -0,0 +1,17 @@
+print (f'inciso A')
+lista = []
+print(lista)
+contacto1 = 'mario:2281818772'
+contacto2 = 'luis:2281818181'
+contacto3= 'jesus:5512345678'
+print(f'ejercicio 1')
+lista = [contacto1, contacto2, contacto3]
+print(lista)
+print(f'ejercicio 2')
+lista.insert (1, 'xavi: 123456789')
+print(lista)
+print(f'ejercicio 3')
+lista.remove('luis:2281818181')
+print(lista)
+print(f'ejercicio 4')
+print(lista[0])
--- python2/operadores.py
+++ python2/operadores.py
@@ -1,1 +1,10 @@
-float input(f'digita un numero ')
+x = float (input(f'digita un numero '))
+y = float (input(f'digita un numero '))
+multi = x * y
+print (f'la multiplicacion de los sumeros ingresados es:  {multi}')
+"""
+Operadores logicos
+AND -> si alguno de los valores es falso responde falso
+OR -> si alguno de los valores es verdadero responde verdadero
+NOT -> convierte el valor de verdadero a falso
+"""
