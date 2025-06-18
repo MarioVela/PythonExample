9d7665813cf0192ad94e4f9918856920e3295319
MarioVela <mariovela22@hotmail.com>
2025-06-17 22:19:54 -0600

    x

---
+++ python3/estructura_de_control.py
@@ -0,0 +1,5 @@
+number = int (input(f'Ingrese un numero para verificar si es par o impar '))
+if number % 2 == 0:
+    print (f'El numero que ingreso es "PAR"')
+else:
+    print (f'El numero que ingreso es "IMPAR"')
