3bc5ffadd4922d71efc24ba7ec28d29837938dcb
MarioVela <mariovela22@hotmail.com>
2025-06-23 20:44:36 -0600

    numero para o impar

---
+++ python3/numero_para_e_impar.py
@@ -0,0 +1,8 @@
+number = int (input(f'tecle un numero '))
+number1 = number / 2
+modulo = number % 2
+xx = modulo == 0
+if xx == True:
+    print (f'el numero que ingreso es par')
+else:
+    print (f'el numero que ingreso es impar')
