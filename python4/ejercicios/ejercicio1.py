f91dc3ddf4c21fae126d3554e64bb6eabdad64d0
MarioVela <mariovela22@hotmail.com>
2025-06-30 22:04:35 -0600

    ejercicio 1 listo, ejercicios 2 en proceso

---
+++ python4/ejercicios/ejercicio1.py
@@ -0,0 +1,7 @@
+
+lista = [9,11,15,2,7]
+for lista in lista:
+    if lista <= 10:
+        print (f'El numero {lista} verificado es menor que 10')
+    else:
+         print (f'El numero {lista} verificado es mayor que 10')
---
+++ python4/ejercicios/ejercicio2.py
@@ -0,0 +1,8 @@
+ciudades = ["veracruz", "mexico", "oaxaca", "xalapa,", "monterrey"]
+letraIni = ciudades[0]
+letraIni2 = letraIni[0]
+for ciudades in ciudades:
+    if ciudades == ["mexico"]:
+        print(f' la ciudad {letraIni} empienza con la leta m')
+    else:
+        print(f'la ciudad {letraIni} no empienza con la leta m')
