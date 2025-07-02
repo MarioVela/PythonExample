4985763b786b2fee046bb5d5fd20c930fecdff38
MarioVela <mariovela22@hotmail.com>
2025-07-01 20:46:33 -0600

    observacion lista

--- python4/ejercicios/ejercicio1.py
+++ python4/ejercicios/ejercicio1.py
@@ -1,6 +1,6 @@
 
-lista = [9,11,15,2,7]
-for lista in lista:
+lista1 = [9,11,15,2,7]
+for lista in lista1:
     if lista <= 10:
         print (f'El numero {lista} verificado es menor que 10')
     else:
