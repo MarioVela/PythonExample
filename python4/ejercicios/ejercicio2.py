88913af04090012fdf291d6191072c8da2fa0f80
MarioVela <mariovela22@hotmail.com>
2025-07-01 20:44:46 -0600

    ejercicio 2 terminado

--- python4/ejercicios/ejercicio2.py
+++ python4/ejercicios/ejercicio2.py
@@ -1,8 +1,8 @@
-ciudades = ["veracruz", "mexico", "oaxaca", "xalapa,", "monterrey"]
-letraIni = ciudades[0]
-letraIni2 = letraIni[0]
-for ciudades in ciudades:
-    if ciudades == ["mexico"]:
-        print(f' la ciudad {letraIni} empienza con la leta m')
+ciudades = ["mveracruz", "mexico", "oaxaca", "xalapa,", "monterrey"]
+i = ciudades[0][0]
+print(i)
+for ciudad in ciudades:
+    if ciudad[0] == "m":
+        print(f' la ciudad {ciudad} empienza con la leta m')
     else:
-        print(f'la ciudad {letraIni} no empienza con la leta m')
+        print(f'la ciudad {ciudad} no empienza con la leta m')
