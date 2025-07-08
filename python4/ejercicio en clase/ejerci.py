733800be77dfddde5160a29792fde874a0460824
MarioVela <mariovela22@hotmail.com>
2025-07-07 20:40:52 -0600

    ejercicio 3

---
+++ python4/ejercicio en clase/ejerci.py
@@ -0,0 +1,21 @@
+from PythonExample.python1.Ejercicio2 import nombre, apellido_m
+from PythonExample.python1.Ejercio1 import alumno
+'''
+id
+nombre
+apellido_p
+apellido_m
+escolaridad
+nacionalidad
+edad
+profersion
+salud: IMC, peso, talla, tipo de sangre
+
+el control escolar de una universidad, requiere:
+regritar nuevos alumnos
+consulta alumnos por id 
+modificar datos del alumno
+eliminar alumno
+calcular edades de acuerdo a la fecha de hacimiento
+calcular el IMC cuando se registra el alumno
+'''
---
+++ python4/ejercicios/ejercicio3.py
@@ -0,0 +1,6 @@
+lista1 = [2, 5, 6, 4, 11,18, 20]
+for lista in lista1:
+    if lista % 2 == 0:
+        print(f'El numero {lista} es divisible entre 2')
+    else:
+        print(f'El numero {lista} no es divisible entre 2')
