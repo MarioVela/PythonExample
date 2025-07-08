70619ccb677559d736c5ae70f3afd5606dd8f786
MarioVela <mariovela22@hotmail.com>
2025-07-07 21:07:50 -0600

    x

---
+++ python4/ejercicios/ejercicio4.py
@@ -0,0 +1,11 @@
+from PythonExample.python1.Ejercio1 import clave
+
+frutas = {
+    "manzana": "roja",
+    "pera": "verde",
+    "naranja": "naranja",
+    "mango": "amarillo",
+    "uva": "morado"
+}
+for fruta in frutas:
+    print(fruta.manzana)
